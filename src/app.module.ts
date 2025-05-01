df084aa3fdea733f3bce2fe26b76281edc8f714e
mira-zoabi <zoabimira@gmail.com>
2025-05-01 16:35:03 +0300

    fisrt commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Global, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { StudentsModule } from './students/students.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      charset: 'utf8mb4',
+      entities: [__dirname + '/../**/entities/*.js'],
+    }),
+    StudentsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
